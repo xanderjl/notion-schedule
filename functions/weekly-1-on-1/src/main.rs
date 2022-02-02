3b64f5cb0fc2cd25ebb309c07abec19b57dea1b2
Xander Low <xander@xandydandy.com>
2022-02-01 22:29:16 -0800

    first commit

---
+++ functions/weekly-1-on-1/src/main.rs
@@ -0,0 +1,39 @@
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use http::header::HeaderMap;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let auth_key = env::var("NOTION_AUTH_TOKEN").unwrap();
+    println!("{}", auth_key);
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn my_handler(
+    event: ApiGatewayProxyRequest,
+    _ctx: Context,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let path = event.path.unwrap();
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(format!("Hello from '{}'", path))),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
+}
