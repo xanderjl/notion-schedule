f82fabc113de78203d3f99f44740a3c6e8201050
Xander Low <xander@xandydandy.com>
2022-02-03 12:17:31 -0800

    chore: write notion db handler

--- functions/weekly-1-on-1/src/main.rs
+++ functions/weekly-1-on-1/src/main.rs
@@ -3,6 +3,7 @@ use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyRes
 use http::header::HeaderMap;
 use lambda_runtime::{handler_fn, Context, Error};
 use log::LevelFilter;
+use notion::NotionApi;
 use simple_logger::SimpleLogger;
 use std::env;
 
@@ -13,12 +14,31 @@ async fn main() -> Result<(), Error> {
         .init()
         .unwrap();
 
+    let db_id = "dfbbdbdb74104b2591b341056e7715bf";
+
     let auth_key = env::var("NOTION_AUTH_TOKEN").unwrap();
     println!("{}", auth_key);
 
     let func = handler_fn(my_handler);
     lambda_runtime::run(func).await?;
     Ok(())
+}
+
+async fn fetch_hour_tracker(
+    event: ApiGatewayProxyRequest,
+    _ctx: Context,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let path = event.path.unwrap();
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(format!("Hello from '{}'", path))),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
 }
 
 async fn my_handler(
